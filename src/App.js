9cd6b78447d155adc38e28f7cfdc5ae578aad9a2
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2022-11-01 13:18:46 +0100

    removing useless stuff

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import FirstPart from './pages/FirstPart';
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Routes>
-        <Route path='/' element={<FirstPart />} />
-      </Routes>
-    </BrowserRouter>
+    <FirstPart />
   );
 }
 
